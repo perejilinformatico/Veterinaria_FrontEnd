fc87febad3b504fbf71e31a142deab95a49734e9
Santiago <perejilinformatico@gmail.com>
2026-01-09 18:13:26 -0300

    Css Sin Terminar

--- src/script.js
+++ src/script.js
@@ -2,5 +2,8 @@ const s = document.getElementsByTagName("button")[0];
 const x = document.getElementsByTagName("button")[1];
 const d = document.getElementsByTagName("button")[2];
 s.addEventListener("click", () => {
-    window.location("#citas")
+    window.location.href = "#citas";
+});
+x.addEventListener("click", () => {
+    window.location.href = "#servicios"
 });
