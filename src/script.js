4e75f605ba328540fea67fd659f0b0c72b6f06ed
Santiago <perejilinformatico@gmail.com>
2026-01-08 21:33:09 -0300

    Cambios sin terminar en css y en js

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const s = document.getElementsByTagName("button")[0];
+const x = document.getElementsByTagName("button")[1];
+const d = document.getElementsByTagName("button")[2];
+s.addEventListener("click", () => {
+    window.location("#citas")
+});
